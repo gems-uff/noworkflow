e1e67f40a29da57518db1f458668984c3e1f9e8d
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2013-09-10 16:43:30 -0400

    Adding some prototype scripts

---
+++ src/test.py
@@ -0,0 +1,22 @@
+import math
+import random
+
+a = 1
+b = 2
+c = 3
+
+print a,b,c
+
+a = "test"
+
+print a,b,c
+
+print random.choice("random")
+
+ 
+ 
+x = []
+x.append(a)
+x.insert(0, 2)
+ 
+print x
