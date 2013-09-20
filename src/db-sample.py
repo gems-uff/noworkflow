e7a00734ed69d581498e1476a007a4d6bef6aaa0
Vanessa Braganholo Murta <vanessa@ic.uff.br>
2013-09-20 11:36:25 -0400

    Moved weather example to \test, created sqlite3 example
    The insert statement of the example is not working… (to be fixed)

---
+++ src/db-sample.py
@@ -0,0 +1,41 @@
+import os
+import sqlite3
+
+#see tutorial at http://pymotw.com/2/sqlite3/
+
+db_filename = 'noworkflow.db'
+db_script = 'noworkflow.sql'
+
+db_is_new = not os.path.exists(db_filename)
+
+conn = sqlite3.connect(db_filename)
+
+if db_is_new:
+    print 'Creating database schema...'
+    with open(db_script, 'rt') as f:
+        schema = f.read()
+        conn.executescript(schema)
+    
+    print 'Inserting sample data...'
+    # nao esta funcionando por algum motivo...
+    conn.execute("""
+    insert into test (id, data) 
+    values (1, 'row 1') 
+    """)
+else:
+    print 'Database exists, assuming schema does, too.'
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    select id, data from test
+    """)
+
+    print 'Retrieving data...'
+    for colinfo in cursor.description:
+        print colinfo
+        
+    for row in cursor.fetchall():
+        #id, data = row
+        print '%2d %s' % (id, data)
+
+conn.close()
---
+++ src/noworkflow.sql
@@ -0,0 +1,4 @@
+create table test (
+	id integer primary key not null,
+	data text
+)
