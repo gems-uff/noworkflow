e4ac2e9a2553bad1d7481512db7cde7ad1beaa99
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2013-10-09 20:05:55 -0400

    using a stack for tracing calls and capturing file acess

--- src/noworkflow/persistence.py
+++ src/noworkflow/persistence.py
@@ -9,6 +9,7 @@ CONTENT_DIRNAME = 'db'
 
 provenance_dir = ''
 content_dir = ''
+std_open = open  # This is changed by retrospective provenance collector at runtime
 
 storage = {}  # Logical store
 
@@ -17,6 +18,7 @@ def connect(script_dir):
     provenance_dir = os.path.join(script_dir, PROVENANCE_DIRNAME)        
     content_dir = os.path.join(provenance_dir, CONTENT_DIRNAME)
      
+
 def put(content):
     content_hash = hashlib.sha1(content).hexdigest()
     content_dirname = os.path.join(content_dir, content_hash[:2])
@@ -24,18 +26,29 @@ def put(content):
         os.makedirs(content_dirname)
     content_filename = os.path.join(content_dirname, content_hash[2:])
     if not os.path.isfile(content_filename):
-        with open(content_filename, "wb") as content_file:
+        with std_open(content_filename, "wb") as content_file:
             content_file.write(content)
     return content_hash
 
+
 def get(content_hash):
     content_filename = os.path.join(content_dir, content_hash[:2], content_hash[2:])
-    with open(content_filename, 'rb') as content_file:
+    with std_open(content_filename, 'rb') as content_file:
         return content_file.read()
+
 
 def store(name, data):
     global storage
     storage[name] = data
+    
+
+def register_file_access(name, mode = 'r', buffering = 'default'):
+    'registers an access to a file'
+    with std_open(name, 'rb') as f:
+        content_hash = put(f.read())
+        # TODO: store the content hash together with access type in the current context and its call stack
+        
+
     
 
         
--- src/noworkflow/retrospective.py
+++ src/noworkflow/retrospective.py
@@ -4,8 +4,37 @@ import sys
 import os
 import inspect
 import utils
+import persistence
+import __builtin__
+import datetime
 
 script = None
+call_stack = []
+CURRENT = -1
+
+def new_open(old_open):
+    'Wraps the open buildin function to register file access'    
+    def wrapper(name, *args, **kwargs):
+        with old_open(name, 'rb') as f:
+            file_access = {  # Create a file access object with default values
+                'name': name,
+                'mode': 'r',
+                'buffering': 'default',
+                'content_before': persistence.put(f.read()),
+                'timestamp': datetime.datetime.now()
+            }
+        file_access.update(kwargs)  # Update with the informed keyword arguments
+        # Update with the informed positional arguments
+        if len(args) > 0:
+            file_access['mode'] = args[0]
+        elif len(args) > 1:
+            file_access['buffering'] = args[1]
+
+        call_stack[CURRENT]['file_access'].append(file_access)
+        utils.print_msg('file {name} accessed in mode {mode}'.format(**file_access))
+        return old_open(name, *args, **kwargs)
+
+    return wrapper
 
 def identify(frame):
     path = frame.f_code.co_filename
@@ -17,14 +46,39 @@ def identify(frame):
     return (filename, codename, line)
 
 def tracer(frame, event, arg):
-    if event == "c_call" and frame.f_code.co_filename == script:
-        namespace = []
-        if arg.__self__ != None:
-            namespace.append(type(arg.__self__).__name__)
-        namespace.append(arg.__name__)
-        print identify(frame), '--call-->', '.'.join(namespace)
-    elif event == "call" and frame.f_back.f_code.co_filename == script:
-        print identify(frame.f_back), '--call-->', identify(frame)
+    if script == frame.f_code.co_filename:
+        if event == 'c_call':
+            call = {
+                'name': arg.__name__ if arg.__self__ == None else '.'.join([type(arg.__self__).__name__, arg.__name__]),
+                'line': frame.f_lineno,
+                'start': datetime.datetime.now(),
+                'file_access': []
+            }
+            call_stack.append(call)
+            utils.print_msg('call to {name} at {start}'.format(**call))  
+        elif event == 'c_return':
+            call = call_stack.pop()
+            call['finish'] = datetime.datetime.now()
+            # TODO: update content of written files
+            # TODO: store the call in the database
+            utils.print_msg('return from {name} at {finish}'.format(**call))
+    if script in [frame.f_code.co_filename, frame.f_back.f_code.co_filename]:  # TODO: should not trace wrapper call, but should trace open. Is it enough renaming?
+        if event == "call":
+            call = {
+                'name': frame.f_code.co_name if frame.f_code.co_name != '<module>' else frame.f_code.co_filename,
+                'line': frame.f_back.f_lineno,
+                'start': datetime.datetime.now(),
+                'file_access': []
+            }
+            call_stack.append(call)
+            utils.print_msg('call to {name} at {start}'.format(**call))  
+        elif event == 'return':
+            call = call_stack.pop()
+            call['finish'] = datetime.datetime.now()
+
+            # TODO: update content of written files
+            # TODO: store the call in the database
+            utils.print_msg('return from {name} at {finish}'.format(**call))
 
     
 #     if (frame.f_code.co_filename == script or frame.f_back.f_code.co_filename == script):
@@ -51,11 +105,13 @@ def tracer(frame, event, arg):
 def enable(args):
     global script
     script = args.script
-#     sys.settrace(tracer)
+    persistence.std_open = open
+    __builtin__.open = new_open(open)
     sys.setprofile(tracer)
     
 def disable():
-    sys.settrace(None)
+    sys.setprofile(None)
+    __builtin__.open = persistence.std_open
 
 # Processor load. Should be collected from time to time (there are static and dynamic metadata)
 # print os.getloadavg()
