4a6764d709757bff79639739531c9f1a6f8d5780
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2014-05-13 09:16:51 -0300

    fixing the setup file.

--- capture/setup.py
+++ capture/setup.py
@@ -5,17 +5,11 @@ use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "noworkflow",
-    version = "0.2",
-    packages = find_packages(),
-    include_package_data = True,
-    
-    entry_points = {
-        'console_scripts': [
-            'now = noworkflow.now:main'
-        ]
-    },
-
-    author = "Leonardo Murta, Vanessa Braganholo, Juliana Freire",
+    version = "0.3",
+    packages = ['noworkflow'],
+    package_data = {'noworkflow': ['resources/*']},
+    entry_points = {'console_scripts': ['now = noworkflow.now:main']},
+    author = "Leonardo Murta, Vanessa Braganholo, Fernando Chirigati, David Koop, and Juliana Freire",
     author_email = "leomurta@ic.uff.br",
     description = "Supporting infrastructure to run scientific experiments without a scientific workflow management system.",
     license = "MIT",
