ab883553486e73edcf039091eac2ec3225d639eb
Joao Felipe <joaofelipenp@gmail.com>
2014-08-26 13:41:53 -0300

    Changing version number to 0.3.1-dev to allow the identification

--- capture/setup.py
+++ capture/setup.py
@@ -5,7 +5,7 @@ use_setuptools()
 from setuptools import setup
 setup(
     name = "noworkflow",
-    version = "0.3",
+    version = "0.3.1-dev",
     packages = ['noworkflow'],
     package_data = {'noworkflow': ['resources/*']},
     entry_points = {'console_scripts': ['now = noworkflow.now:main']},
