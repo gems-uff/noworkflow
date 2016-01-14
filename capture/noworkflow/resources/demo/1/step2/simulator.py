7ab69c8ebbe7e9b404a7d5b6d78fad02e8eab9c8
Joao Felipe <joaofelipenp@gmail.com>
2016-01-14 10:22:02 -0200

    #71: fix demo. Version  0.15.1

--- capture/noworkflow/resources/demo/1/step2/simulator.py
+++ capture/noworkflow/resources/demo/1/step2/simulator.py
@@ -1,6 +1,6 @@
 import time
 
 def simulate(a, b):
-    time.sleep(0.05)
+    time.sleep(0.01)
     data = a + b
     return data
--- capture/noworkflow/resources/demo/1/step3/simulation_complete.py
+++ capture/noworkflow/resources/demo/1/step3/simulation_complete.py
@@ -3,28 +3,27 @@ import time
 import tempfile
 import os
 #this code requires matplotlib to be installed
-#please refer to http://matplotlib.org/downloads.html 
+#please refer to http://matplotlib.org/downloads.html
 import matplotlib.pyplot as plt
-    
+
 class TabularData:
     def __init__(self, data, header=None):
         self.data = data
         self.header = header
 
 ###############################################################
-        
+
 def simulation(dataA, dataB):
-    #TODO: trocar aqui para 20 novamente  
-    time.sleep(2)
-    
+    time.sleep(0.2)
+
     all_data = dataA.data + dataB.data
-    
+
     (f, name) = tempfile.mkstemp(prefix='vtweather')
     os.close(f)
-    
+
     writer = csv.writer(open(name, 'w'), delimiter=':')
     writer.writerows(all_data)
-    
+
     #res = registry.get_descriptor_by_name('edu.utah.sci.vistrails.basic',
     #    'File').module()
     #res.name = name
@@ -39,12 +38,12 @@ def csvRead(f):
         data.append(row)
     tab_data = TabularData(data, header)
     #self.setResult('data', tab_data)
-    return tab_data    
-        
+    return tab_data
+
 def extractColumn(d, column_name, column):
         data = d.data
         header = d.header
-        
+
         #Test to check whether column_name is not empty
         if column_name:
             if header is None:
@@ -56,24 +55,24 @@ def extractColumn(d, column_name, column):
                 print("Data does not contain column" + column)
         else:
                 idx = column
-            
+
         col_data = []
         for row in data:
             print('processing row', row)
             col_data.append(row[idx])
         return col_data
-  
+
 def mplScatter(x,y):
-    kwargs = {}        
+    kwargs = {}
     #kwargs['y'] = y
     #kwargs['x'] = x
-    plt.scatter(x, y, s=20, c='b', marker='o', cmap=None, norm=None, 
+    plt.scatter(x, y, s=20, c='b', marker='o', cmap=None, norm=None,
                     vmin=None, vmax=None, alpha=None, linewidths=None, verts=None, **kwargs)
     plt.xlabel('Temperature')
     plt.ylabel('Precipitation')
-    plt.show()
-    
-######################################################################################    
+    plt.savefig("output.png")
+
+######################################################################################
 #Main Program
 print('Reading data data1.dat...')
 dataA = csvRead('data1.dat')
@@ -94,8 +93,6 @@ columnX = extractColumn(d, '', 0)
 #Transform column values in a list of float
 print('Converting values...')
 out1 = [float(i) for i in columnX]
-out2 = [float(i) for i in columnY] 
+out2 = [float(i) for i in columnY]
 print('Generating result...')
 mplScatter(out1, out2)
-
-
