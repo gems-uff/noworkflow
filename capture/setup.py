5a361c499680ee2c32ed8960c2d10e32fa074580
Joao Felipe <joaofelipenp@gmail.com>
2016-02-24 03:08:14 -0300

    Setup classifiers

--- capture/setup.py
+++ capture/setup.py
@@ -119,4 +119,19 @@ setup(
         "install": CustomInstallCommand,
         "develop": CustomDevelopCommand,
     },
+    classifiers={
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    }
 )
