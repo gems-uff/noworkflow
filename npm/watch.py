b70bc6382e50f7f3f421af216d6a778564e49c38
João Felipe Pimentel <joaofelipenp@gmail.com>
2017-10-04 14:59:59 -0300

    Add watcher to compile typescript packages automatically

---
+++ npm/watch.py
@@ -0,0 +1,83 @@
+import os
+import subprocess
+from pathlib import Path
+from time import sleep
+
+PACKAGES = Path('packages')
+
+class Module:
+
+    def __init__(self, name, path=None, files=None, dependencies=None):
+        self.name = name
+        if path is None:
+            path = PACKAGES / name
+        self.path = path
+        self.files = files or ["src/", "style/"]
+        self.dependencies = dependencies or []
+        self.old_sum = 0
+        self.check_dir()
+
+    def check_dir(self):
+        """Check if a file has changed in the package"""
+        time_list = []
+        for file in self.files:
+            file_list = []
+            file_path = self.path / Path(file)
+            if not file.endswith("/"):
+                file_list = [file_path]
+            else:
+                for root, _, files in os.walk(file_path):
+                    root = Path(root)
+                    file_list = [root / f for f in files]
+
+            time_list += [os.stat(f).st_mtime for f in file_list]
+
+        new_sum = sum(time_list)
+        result = new_sum != self.old_sum
+        self.old_sum = new_sum
+        return result
+
+    def check(self, run=True, visited=set()):
+        """Check if the module or its dependencies has changed"""
+        if self in visited:
+            return True
+        visited.add(self)
+        invalid = False
+        for dependency in self.dependencies:
+            if not dependency.check(run, visited):
+                invalid = True
+
+        invalid |= self.check_dir()
+
+        if run and invalid:
+            print("Building", self.name)
+            process = subprocess.Popen(
+                "npm run build",
+                shell=True,
+                cwd=self.path,
+            )
+
+            status = process.wait()
+            if status:
+                raise Exception("NPM run failed")
+
+        return not invalid
+
+    def __hash__(self):
+        return hash(self.path)
+
+    def __repr__(self):
+        return "Module({})".format(self.name)
+
+
+utils = Module("utils")
+history = Module("history", dependencies=[utils])
+trial = Module("trial", dependencies=[utils])
+nowvis = Module("nowvis", dependencies=[history, trial])
+
+
+print("Monitoring packages...")
+while True:
+    visited = set()
+    nowvis.check(visited=visited)
+    sleep(1.0)
