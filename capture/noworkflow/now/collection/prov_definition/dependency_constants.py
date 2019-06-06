ef76697d25d232194056db8b4c98c1a71b6a5cc9
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2019-06-06 13:31:45 -0300

    Define code component names and dependency types

---
+++ capture/noworkflow/now/collection/prov_definition/dependency_constants.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2019 Universidade Federal Fluminense (UFF)
+# Copyright (c) 2019 Polytechnic Institute of New York University.
+# This file is part of noWorkflow.
+# Please, consult the license terms in the LICENSE file.
+"""Define dependency constants"""
+
+ARGUMENT = "argument"
+#  def process_default
+ASSIGN = "assign"
+#  def visit_assign
+#  def visit_annassign
+BASE = "base"
+#  def visit_ClassDef
+CONDITION = "condition"
+#  def visit_While
+#  def visit_If
+DECORATE = "decorate"
+#  def visit_FunctionDef
+#  def visit_ClassDef
+DECORATOR = "decorator"
+#  def process_decorator
+DEPENDENCY = "dependency"
+#  def visit_ClassDef
+#  def visit_Print
+#  def visit_For
+#  def visit_Exec
+#  def capture
+USE = "use"
+#  def process_decorator
+#  def visit_Return
+
+
+
+# def visit_augassign:
+#   add_assign
+#   mult_assign
+#   div_assign
+#   floordiv_assign
+#   pow_assign
+#   sub_assign
+#   mod_assign
+#   matmult_assign
+#   ...
