c279f3b04c169652b0daef2795844f545a2e504d
jaglima <jesseamerico@gmail.com>
2023-08-28 21:55:22 -0300

    First version with new exp_compare

--- capture/noworkflow/now/tagging/var_tagging.py
+++ capture/noworkflow/now/tagging/var_tagging.py
@@ -10,8 +10,10 @@ from __future__ import (absolute_import, print_function,
 from noworkflow.now.persistence.models import Evaluation
 from noworkflow.now.models.dependency_querier import DependencyQuerier
 from noworkflow.now.models.dependency_querier.querier_options import QuerierOptions
+import ipdb
 
 class NotebookQuerierOptions(QuerierOptions):
+    import numpy as np
     global body_function_def
     dep_list = []
     
@@ -20,6 +22,7 @@ class NotebookQuerierOptions(QuerierOptions):
         self.level = level
     
     def visit_arrow(self, context, neighbor):
+        import numpy as np
         # keeping 
         if neighbor.evaluation.code_component.type == 'function_def':
             body_function_def.append(int(neighbor.evaluation.code_component.id))
@@ -30,24 +33,39 @@ class NotebookQuerierOptions(QuerierOptions):
         context_code_comp = context.evaluation.code_component
         neighbor_code_comp = neighbor.evaluation.code_component
         
-        if context_code_comp.type == 'literal' and neighbor_code_comp.type == 'type':
-            self.dep_list.append((str(context.evaluation.checkpoint), str(context.evaluation.id), context_code_comp.name, context.evaluation.repr))
+        if context_code_comp.type == 'literal' and neighbor_code_comp:
+            self.dep_list.append(( context_code_comp.name, context.evaluation.repr))
         elif neighbor.arrow not in arrow_list:
                 if context_code_comp.type not in type_list:
                     if neighbor_code_comp.type not in type_list:
                         if not (neighbor.arrow == 'use' and context_code_comp.type == 'call'):
                             if (neighbor_code_comp.container_id != None):
                                 if neighbor_code_comp.container_id not in body_function_def or self.level:
-                                    #if len(context.evaluation.repr) > 0:  # arbitrary lenght to avoid matricial outputs
-                                    self.dep_list.append((str(context.evaluation.checkpoint), str(context.evaluation.id), context_code_comp.name, context.evaluation.repr))
-                                    #else:
-                                    #    self.dep_list.append((str(context.evaluation.checkpoint), str(context.evaluation.id), context_code_comp.name, 'matrix'))
+                                    #ipdb.set_trace()
+                                    if len(context.evaluation.repr) > 20:  # arbitrary lenght to avoid matricial outputs
+                                        dimensions = np.frombuffer(context.evaluation.repr.encode(), dtype=np.uint8)
+                                        self.dep_list.append((str(context_code_comp.name), str('matrix dim' + str(dimensions.shape))))
+                                    else:
+                                        self.dep_list.append((str(context_code_comp.name), str(context.evaluation.repr)))
 
     def predecessors_output(self):
         global dep_dict
         
-        dep_dict = {i[0] : i[1] for i in enumerate(self.dep_list)}
-        return dep_dict
+        # remove duplicated keys. No better solution up to now 
+        unique_elements = []
+        seen_elements = set()
+
+        for element in self.dep_list:
+            if element not in seen_elements:
+                unique_elements.append(element)
+                seen_elements.add(element)     
+        
+        # remove duplicated values
+        elements = [tuple_ for tuple_ in unique_elements if tuple_[0] != tuple_[1]]
+
+        # create an enumerated dictionary        
+        dep_dict = {i[0] : i[1] for i in reversed(list(enumerate(elements)))}
+        return dep_dict, self.dep_list
 
 def now_tag(tag):
    """Tags a given cell"""
@@ -97,19 +115,49 @@ def get_pre(var_name, glanularity = False):
     
     return nbOptions.predecessors_output()   
 
-def exp_compare(trial_a, trial_b):
+def dict_to_text(op_dict):
+    import textwrap
+
+    # Convert dictionary to plain text with each key-value pair on a separate row
+    plain_text = ""
+    
+    for key, value_list in op_dict.items():
+        values_text = ", ".join(map(str, value_list))
+        key_value_pair = f"{values_text}"
+        wrapped_lines = textwrap.fill(key_value_pair, subsequent_indent='    ')
+        plain_text += wrapped_lines + "\n"
+
+    return plain_text
+
+def exp_compare(trial_a, trial_b, html=False):
     import shelve
+    import difflib
+    from IPython.display import HTML
     
-    # Retrieve the dictionary a from the shelve file
+    # Retrieve the ops dictionary from the shelve file
     with shelve.open('ops') as shelf:
-        dict_a = shelf['dict_a']
-        print("Retrieved dictionary:", dict_a)
-        dict_b = shelf['dict_b']
-        print("Retrieved dictionary:", dict_b)
-        
-    # comparing two dicts
+        dict1 = shelf[trial_a]
+        dict2 = shelf[trial_b]
     
-    return dict_a == dict_b
+    if not html:
+        return dict1, dict2
+    else:
+        plain_text_a = dict_to_text(dict1)
+        plain_text_b = dict_to_text(dict2)
+
+        # Original and modified code strings
+        original_code = plain_text_a
+        modified_code = plain_text_b
+
+        # Calculate the differences using difflib
+        differ = difflib.HtmlDiff()
+        diff_html = differ.make_table(
+            original_code.splitlines(),
+            modified_code.splitlines(),
+            context=False,  # Show some context lines around changes
+            numlines=0     # Number of lines of context to show
+        )
+        display(HTML(diff_html))
 
 def store_operations(trial, ops_dict):
     import shelve
@@ -151,7 +199,10 @@ def exp_compare(trial_a, trial_b):
             print(f"Key '{key}': Values are equal")
         else:
             print(f"Key '{key}': Values are different")
-            print("->>>", values1[2:4], values2[2:4])
+            if len(values1[3]) > 10 or len(values2[3]) > 10:
+                print("->>>", values1[2:4][:3], values2[2:4][:3])
+            else:
+                print("->>>", values1[2:4], values2[2:4])
 
 def get_pre_all(glanularity = False):
     from noworkflow.now.persistence.models.stage_tags import StageTags
