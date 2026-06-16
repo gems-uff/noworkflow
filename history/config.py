89e5454b5eaf6fa1e3b7f435fa36e39ca992de50
Arthur Paiva <arthurpaivasg@yahoo.com.br>
2026-06-15 21:42:39 -0300

    Updates timeline

--- history/config.py
+++ history/config.py
@@ -32,4 +32,68 @@ configuration = {
         Paper("PIMENTEL, J. F.; MURTA, L. G. P.; BRAGANHOLO, V.; FREIRE, J.; noWorkflow: a Tool for Collecting, Analyzing, and Managing Provenance from Python Scripts. In: International Conference on Very Large Data Bases (VLDB), 2017, Munich, Germany.",
               "https://github.com/gems-uff/noworkflow/raw/master/docs/vldb2017.pdf"),
     ],
+    (2017, 12, 11): [
+        Blank(),
+        Author("vyniciuspontes.jpg", "Vynicius Pontes", "pontes.vynicius@gmail.com")
+    ],
+    (2018, 6, 7): [
+        Author("ruslanguerra.jpg","Ruslan Guerra", "ruslan.guerra88@gmail.com")
+    ],
+    (2019, 6, 25): [
+        Blank(),
+        Blank(),
+        Author("daniel.jpg", "Daniel", "daniel_toledo02@hotmail.com")
+    ],
+    (2019, 4, 14): [
+        Author("eduardojandre.jpg", "Eduardo Jandre", "eduardojandre@gmail.com")
+    ],
+    (2019, 6, 17): [
+        Blank(),
+        Author("erickgrilo.jpg", "Erick Grilo", "simasgrilo86@gmail.com")
+    ],
+    (2021, 1, 1): [
+        Paper("LEMOS, V.; ProspectiveProv: Proveniência Prospectiva de Experimentos Representados por Scripts, Universidade Federal Fluminense, 2021.",
+              "https://github.com/gems-uff/noworkflow/blob/master/docs/Dissertacao_Vitor_Lemos.pdf")
+    ],
+    (2022, 1, 1): [
+        Paper("OLIVEIRA, E.; Enabling Collaboration in Scientific Experiments. Masters Dissertation, Universidade Federal Fluminense, 2022.",
+              "https://github.com/gems-uff/noworkflow/blob/master/docs/Disserta__o___Eduardo_Jandre.pdf")
+    ],
+    (2022, 1, 4): [
+        Blank(),
+        Author("henriquelinhares.jpg", "Henrique Linhares", "linhares.h@gmail.com")
+    ],
+    (2022, 10, 3): [
+        Paper("LEMOS, Vitor Gama et al. P+ RProv: Prospective+ Retrospective Provenance Graphs of Python Scripts. Journal of Information and Data Management, v. 13, n. 4, 2022.",
+              "https://journals-sol.sbc.org.br/index.php/jidm/article/view/2059")
+    ],
+    (2022, 10, 3): [
+        Author("vitorlemos.png", "Vitor Gama Lemos", "vitorlemos@ic.uff.br")
+    ],
+    (2023, 3, 29): [
+        Blank(),
+        Author("arthurpaiva.png", "Arthur Paiva", "arthurpaivasg@yahoo.com.br")
+    ],
+    (2023, 9, 20): [
+        Blank(),
+        Blank(),
+        Author("jesse.jpg", "Jessé Lima", "jesseamerico@gmail.com")
+    ],
+    (2024, 11, 8): [
+        Blank(),
+        Author("anacarvalho.jpg", "Ana Caroline Torres de Carvalho", "engcarolinetorres@gmail.com")
+    ],
+    (2024, 7, 4): [
+        Author("joshuatalahatu.jpg", "Joshua Daniel Talahatu", "joshuaminex02@gmail.com")
+    ],
+    (2025, 7, 4): [
+        Blank(),
+        Author("brunosumar.jpg","Bruno Sumar", "sumar.bruno.34@gmail.com")
+    ],
+    (2025, 6, 25): [
+        Author("matheuslobo.jpg", "Matheus Lobo", "matheuslobo@id.uff.br")
+    ],
+    (2026, 3, 22): [
+        Author("felipecoutinho.jpg", "Felipe Coutinho", "felipe_coutinho@id.uff.br")
+    ]
 }
